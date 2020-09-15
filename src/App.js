4755bdb4c0ad6496c26ce47f8c689912df8ed99c
nadinedelia <nadine.delia@yahoo.com>
2020-09-15 12:44:50 +0200

    importing components in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React from 'react'; 
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
 class App extends React.Component {
     render() {
