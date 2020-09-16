294d324d47af464a5169c74842ab0eae616ff287
nadinedelia <nadine.delia@yahoo.com>
2020-09-16 10:45:00 +0200

    displaying bar chart without data when selecting country

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ class App extends React.Component {
     async componentDidMount() {
         const fetchedData = await fetchData();
 
-        this.setState({ data: fetchedData })
+        this.setState({ data: fetchedData });
     }
 
     handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
-        
-        console.log(fetchedData);
+
+        this.setState({ data: fetchedData, country: country });
     }
 
     render() { 
-        const { data } = this.state;
+        const { data, country } = this.state;
         return (
             <div className={styles.container}>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart />
+                <Chart data={data} country={country}/>
             </div>
         );
     }
